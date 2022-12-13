89b822db4386496ff9bbec0c485c058f5c223131
BORNKING16 <ige.adeyemi1993@gmail.com>
2022-12-13 13:33:02 -0800

    corrected

--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,19 @@
-#ifndef _BINARY_TREES_ALX_
-#define _BINARY_TREES_ALX_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
+#include <stdio.h>
 #include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdbool.h>
+#include <limits.h>
+
+
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+/* basic binary tree */
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -19,11 +31,16 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+/* Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
 typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
-/** Func Prototype */
-void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
@@ -44,7 +61,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-													const binary_tree_t *second);
+	const binary_tree_t *second);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
 int binary_tree_is_complete(const binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
@@ -73,4 +90,5 @@ char *convert(unsigned long int num, int base, int lowercase);
 bst_t *swap(bst_t *a, bst_t *b);
 
 size_t _binary_tree_height(const binary_tree_t *tree);
-#endif
+
+#endif /* BINARY_TREES_H */
